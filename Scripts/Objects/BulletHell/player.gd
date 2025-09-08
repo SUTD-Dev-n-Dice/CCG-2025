b58c77273a6a24a481434f9011d20542cba08982
nikelasi <nicholas@nj.sg>
2025-09-09 02:40:19 +0800

    feat(bullet-hell/player): add co-op player attacks

--- Scripts/Objects/BulletHell/player.gd
+++ Scripts/Objects/BulletHell/player.gd
@@ -1,71 +1,150 @@
 extends CharacterBody2D
 
+#region Class Preloads
+const Player = preload("res://Scripts/Objects/BulletHell/Player.gd")
+const PlayerSpike = preload("res://Scripts/Objects/BulletHell/PlayerSpike.gd")
+const PlayerShockwave = preload("res://Scripts/Objects/BulletHell/PlayerShockwave.gd")
+const GameManager = preload("res://Scripts/Objects/BulletHell/GameManager.gd")
+
+const PLAYER_SPIKE_SCENE = preload("res://Scenes/Objects/BulletHell/player_spike.tscn")
+const PLAYER_SHOCKWAVE_SCENE = preload("res://Scenes/Objects/BulletHell/player_shockwave.tscn")
+#endregion
+
 enum PlayerState { GROUNDED, FALLING }
+const PlayerName = {
+	ONE = "Player1",
+	TWO = "Player2",
+}
 
 @export_group("Input Map")
-@export_subgroup("Movement")
 @export var left: StringName
 @export var right: StringName
 @export var up: StringName
 @export var down: StringName
-@export_subgroup("Action")
 @export var jump: StringName
+@export var spike: StringName
+@export var wave: StringName
 
-@onready var game_manager = get_node("/root/BulletHell")
+@export_group("References")
+@export var other: Player
+
+@onready var game_manager: GameManager = get_node("/root/BulletHell")
 
 var healthQuarts: float = 12.0
 var maxHealthQuarters: int = 12
 var canTakeDamage := true
 var damageCooldown := 0.5   # invulnerability timing in seconds
+var shockwave: PlayerShockwave = null
 signal health_changed(player: String, health: float)
 signal game_end
 
-const ORIGINAL_SCALE = Vector2(1, 1)
-const JUMP_SCALE = Vector2(1.5, 1.5)
+#region Shared Internals
+var shared = SharedState.get_instance()
+class SharedState extends RefCounted:
+	static var _instance: SharedState
+	var spike: PlayerSpike
+	
+	func _init():
+		if _instance != null:
+			printerr("attempted creation of second SharedState instance")
+			return
+		_instance = self
+		spike = null
 
+	func create_spike(creator: Player) -> PlayerSpike:
+		if spike != null:
+			return null
+
+		spike = PLAYER_SPIKE_SCENE.instantiate()
+		spike.player = creator
+		spike.tracker = self
+		return spike
+
+	static func get_instance() -> SharedState:
+		if _instance == null:
+			_instance = SharedState.new()
+		return _instance
+#endregion
+
+#region Sprite Internals
+var _sprite_original_scale: float
+var _sprite_scale: float:
+	get:
+		return $Sprite2D.scale.x / _sprite_original_scale
+	set(new_scale):
+		$Sprite2D.z_index = new_scale * 100
+		$Sprite2D.scale.x = _sprite_original_scale * new_scale
+		$Sprite2D.scale.y = _sprite_original_scale * new_scale
+
+func _sprite_init_scale():
+	_sprite_original_scale = $Sprite2D.scale.x
+
+func _sprite_update_state(state: PlayerState):
+	_sprite_scale = {
+		PlayerState.GROUNDED: 1.0,
+		PlayerState.FALLING: 1.5,
+	}[state]
+
+func _sprite_process_delta(delta: float):
+	if state == PlayerState.FALLING:
+		_sprite_scale -= delta
+		if _sprite_scale <= 1.0:
+			update_state(PlayerState.GROUNDED)
+#endregion
+
+#region State Internals
+var running: bool:
+	get: return not game_manager.getState(); # getState returns isEnded
 var state: PlayerState = PlayerState.GROUNDED
 
 func update_state(new_state: PlayerState):
 	state = new_state
-	match state:
-		PlayerState.GROUNDED:
-			$CollisionShape2D.disabled = false
-			scale = ORIGINAL_SCALE
-		PlayerState.FALLING:
-			$CollisionShape2D.disabled = true
-			scale = JUMP_SCALE
+	_sprite_update_state(new_state)
+#endregion
 
-func process_input():
+func _ready():
+	_sprite_init_scale()
+
+func _process_input():
 	# movement
 	var input_direction = Input.get_vector(left, right, up, down).normalized()
 	velocity = input_direction * 400
-	
+
 	# use name as the indicator for current player
-	if name == "Player1":
+	if name == PlayerName.ONE:
 		game_manager.setPlayer1Position(position)
-	elif name == "Player2":
+	elif name == PlayerName.TWO:
 		game_manager.setPlayer2Position(position)
-	
+
 	# jump
-	if (state == PlayerState.GROUNDED and Input.is_action_just_pressed(jump)):
+	if Input.is_action_just_pressed(jump) and state == PlayerState.GROUNDED:
 		update_state(PlayerState.FALLING)
 
+	# spike
+	if Input.is_action_just_pressed(spike) and other != null:
+		var spike = shared.create_spike(self)
+		if spike:
+			game_manager.add_child(spike)
+
+	# shockwave
+	if Input.is_action_just_pressed(wave) and shockwave == null:
+		shockwave = PLAYER_SHOCKWAVE_SCENE.instantiate()
+		shockwave.player = self
+		game_manager.add_child(shockwave)
+
 func _process(delta):
-	if not game_manager.getState():
-		process_input()
+	if not running:
+		return
+
+	_process_input()
 
 func _physics_process(delta):
-	if game_manager.getState():
+	if not running:
 		return
-	
+
 	move_and_slide()
-	if (state == PlayerState.FALLING):
-		scale.x -= delta
-		scale.y = scale.x
-		if (scale <= ORIGINAL_SCALE):
-			update_state(PlayerState.GROUNDED)
-		$Sprite2D.z_index = scale.x * 100
-	
+	_sprite_process_delta(delta)
+
 	# check for damage events
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
@@ -79,14 +158,14 @@ func take_damage():
 	if not canTakeDamage:
 		return
 	canTakeDamage = false
-	
+
 	healthQuarts -= 1.0
 	emit_signal("health_changed", healthQuarts) # connected in game manager to heartsUI
 	print("Player hit! Health now:", healthQuarts)
-	
+
 	if healthQuarts == 0.0:
 		emit_signal("game_end")
-	
+
 	# start cooldown timer
 	var t = get_tree().create_timer(damageCooldown)
 	t.timeout.connect(func(): canTakeDamage = true)
